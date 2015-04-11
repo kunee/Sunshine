e89fc9310e55b1f7b3fdb75b3e00e63f97552072
Rico Konieczek <rico.konieczek@gmail.com>
2015-04-11 10:28:01 +0200

    Added a second view type to the forecast adapter for today

--- app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -18,14 +18,34 @@ public class ForecastAdapter extends CursorAdapter {
         super(context, c, flags);
     }
 
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
+    @Override
+    public int getItemViewType(int position) {
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
+    }
+
+    @Override
+    public int getViewTypeCount() {
+        return VIEW_TYPE_COUNT;
+    }
+
     /*
         Remember that these views are reused as needed.
      */
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
-        return view;
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        if (viewType == VIEW_TYPE_TODAY) {
+            layoutId = R.layout.list_item_forecast_today;
+        } else if (viewType == VIEW_TYPE_FUTURE_DAY) {
+            layoutId = R.layout.list_item_forecast;
+        }
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
     }
 
     /*
